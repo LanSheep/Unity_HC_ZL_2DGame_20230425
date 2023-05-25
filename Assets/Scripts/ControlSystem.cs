f407ca594c43cdc6ee6d7d5f559b2da90e2b09d4
LanSheep <60056925+LanSheep@users.noreply.github.com>
2023-05-25 20:35:17 +0800

    05.25

---
+++ Assets/Scripts/ControlSystem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ControlSystem : MonoBehaviour
+{
+    private void Awake()
+    {
+        print(123);
+        print("1+2");
+        print($"{ 1 + 2}");
+    }
+
+    private void Start()
+    {
+        print("<color=red>開始事件</color>");
+
+    }
+
+    private void Update()
+    {
+        print("<color=#EAC100>更新事件</color>");
+
+    }
+}
--- Assets/Scripts/MenuManger.cs
+++ Assets/Scripts/MenuManger.cs
@@ -1,6 +1,15 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MeanManger : MonoBehaviour
+public class MenuManger : MonoBehaviour
 {
-   
+    public void StartGame()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
