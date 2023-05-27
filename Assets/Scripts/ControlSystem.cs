09a72d5f3cb8fcc9908d0c81e1ad37a0704df53d
LanSheep <60056925+LanSheep@users.noreply.github.com>
2023-05-28 01:45:43 +0800

    05.27 Updata

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -2,22 +2,59 @@
 
 public class ControlSystem : MonoBehaviour
 {
+    [Header("移動速度")]
+    [Range(0, 10)]
+    public float moveSpeed = 3.5f;
+
+    [Header("剛體")]
+    public Rigidbody2D rig;
+
+    [Header("動畫控制器")]
+    public Animator ani;
+
+    [Header("跑步參數")]
+    public string parRun = "walk switch";
+
+    //起始資料
     private void Awake()
     {
-        print(123);
-        print("1+2");
-        print($"{ 1 + 2}");
+        //print(123);
+        //print("1+2");
+        //print($"{ 1 + 2}");
     }
 
     private void Start()
     {
-        print("<color=red>開始事件</color>");
+        //print("<color=red>開始事件</color>");
 
     }
 
     private void Update()
     {
-        print("<color=#EAC100>更新事件</color>");
+        //print("<color=#EAC100>更新事件</color>");
+        //呼叫move
+        Move();
+    }
+
+    private void Move()
+    {
+        //取得控制輸入值
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        //print("水平值:" + h);
+
+        if (h != 0 || v != 0)
+        {
+            //設置物體的速度
+            rig.velocity = new Vector2(h, v) * moveSpeed;
+        }
+        else
+        {
+            ani.CrossFade("girl_ldle", 0);
+            // 過渡到名為 "Run" 的動畫狀態，過渡時間為 0 }
+        }
+        ani.SetBool(parRun, h != 0 || v != 0);
 
     }
 }
